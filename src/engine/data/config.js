2c884601ae9e795ec5e5b2c709b96e38f6949140
JOEL LUCAS LOPES <129183406+srJFRONT@users.noreply.github.com>
2024-11-08 11:32:58 -0300

    Add files via upload

---
+++ src/engine/data/config.js
@@ -0,0 +1,11 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyABCXDyTCj1k-_3siNdzIdni8MxbXYqgzE",
+    authDomain: "projeto-teste-104c8.firebaseapp.com",
+    projectId: "projeto-teste-104c8",
+    storageBucket: "projeto-teste-104c8.appspot.com",
+    messagingSenderId: "117185890573",
+    appId: "1:117185890573:web:d8c6c1ce1993255f320844"
+}
+
+export default firebaseConfig
